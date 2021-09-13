bd0227f46fbbd33eb959472b37330bad9007c31e
Miguel Huayhua <miguelhuayhuac@gmail.com>
2021-09-13 17:44:45 -0400

    segundo intento

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ const fileupload = require('express-fileupload')
 const morgan = require('morgan')
 const cors = require('cors')
 app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 app.use(cors())
 app.use(morgan("dev"))
 app.use(fileupload({ createParentPath: true }))
 app.set("puerto", process.env.PORT || 5000)
+
+app.get("/prueba",(req,res)=>{
+    let data = {
+        nombre:"Miguel",
+        apellido:"Huayhua Condori"
+    }
+    res.json(data)
+})
 
 app.post('/file', (req, res) => {
     let file = req.files.myfile
